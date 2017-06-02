751c62817ddfaca83867bd56eef808f3f0a24cde
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 14:58:42 +0200

    Création du Model ArticleDePresse avec les fonctions de validation

--- public/js/main.js
+++ public/js/main.js
@@ -16,4 +16,29 @@ $(function(){
     testModelMembre.log()
   $(".content").html(JST['membre'](testModelMembre.toJSON()));
 
+
+    //Article de Presse
+
+    var testArticle = new ModelArticleDePresse({
+        url: "Michelle",
+        titreArticle: "Un bolomey dans l'espace"
+    });
+
+    testArticle.log();
+    console.log(testArticle.isValid());
+    testArticle.set("url",123);
+    testArticle.log();
+    console.log(testArticle.isValid());
+    testArticle.set("titreArticle","2"+123);
+    testArticle.set({
+        url: "http://bliche.ch",
+        description: "coucou",
+        date: new Date("2017-03-21"),
+        nomPresse: ""
+    });
+    testArticle.log();
+    console.log(testArticle.isValid());
+    console.log(testArticle.validationError);
+    console.log(testArticle.attributes.date);
+
 });
