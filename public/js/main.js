77fd34460d97e3232b0b327cde34d58bbe1362b3
LEYASKA\Léa <lea.soukouti@heig-vd.ch>
2017-06-02 14:17:45 +0200

    finalisation du model membre

--- public/js/main.js
+++ public/js/main.js
@@ -4,15 +4,15 @@ $(function(){
         adresseMail: "lea.soukouti@gmail.com",
         nom: "Soukouti",
         prenom: "Léa",
-        dateNaissance: "29.03.1994",
+        dateNaissance: "29/03/1994",
         section: "Media",
         description: "je suis trop belle",
         role: "admin",
-        photoProfil:""
+        photoProfil:"moi.jpg"
 
     });
 
-
+    console.log(testModelMembre.validationError);
     testModelMembre.log()
   $(".content").html(JST['membre'](testModelMembre.toJSON()));
 
