7b9c70b102e25147b1d504b57bf08f33eb82373c
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 10:47:34 +0200

    Installed Libraries : Backbonejs, Underscorejs, Handlebarsjs Installed packJs.php

---
+++ public/js/Lib/backbone.hydrotech.js
@@ -0,0 +1,30 @@
+var Hydrotech = {
+  Model: Backbone.Model.extend({
+    log: function (){
+      console.log(this.attributes);
+    },
+    undo: function (){
+      this.attributes = this.previousAttributes();
+    }
+  }),
+  View: Backbone.View.extend({
+    initialize: function (options){
+      this.tmpl = JST[options.template];
+      this.listenTo(this.model, 'change', this.render);
+      this.render();
+    },
+    render: function(){
+      var dom = this.tmpl(this.model.attributes);
+      this.$el.html(dom);
+      return this.$el;
+    }
+  }),
+  ViewCollection: Backbone.View.extend({
+
+  }),
+  Collection: Backbone.Collection.extend({
+    log: function (){
+      console.log(JSON.parse(JSON.stringify(this)));
+    }
+  })
+};
