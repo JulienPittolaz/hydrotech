b6d37a5f9c90a06a0456116fa81349d0b2f9d04c
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 16:38:35 +0200

    Correction des *parse errors* sur le fichier main.js
    Ajout de test pour le nouveau Model Créé

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     // MEMBRE
     var testModelMembre = new ModelMembre({
         adresseMail: "lea.soukouti@gmail.com",
@@ -8,14 +8,14 @@ $(function(){
         section: "Media",
         description: "je suis trop belle",
         role: "admin",
-        photoProfil:"moi.jpg"
+        photoProfil: "moi.jpg"
 
     });
     testModelMembre.isValid();
     console.log("Erreurs Membre: " + testModelMembre.validationError);
     testModelMembre.log()
 
-  $(".content").html(JST['membre'](testModelMembre.toJSON()));
+    $(".content").html(JST['membre'](testModelMembre.toJSON()));
 
     //Article de Presse
 
@@ -29,29 +29,37 @@ $(function(){
 
     testArticle.log();
 
-});
 
 
 // RESEAU SOCIAL
-    var testModelReseauSocial = new ModelReseauSocial({
-        nom : "Face de bouk",
-        url: "facebook.com"
-    })
+var testModelReseauSocial = new ModelReseauSocial({
+    nom: "Face de bouk",
+    url: "facebook.com"
+})
 
-    testModelReseauSocial.isValid();
-    console.log("Erreurs ReseauSocial: " + testModelReseauSocial.validationError);
-    testModelReseauSocial.log()
-    $(".content").html(JST['reseauSocial'](testModelReseauSocial.toJSON()));
+testModelReseauSocial.isValid();
+console.log("Erreurs ReseauSocial: " + testModelReseauSocial.validationError);
+testModelReseauSocial.log()
+$(".content").html(JST['reseauSocial'](testModelReseauSocial.toJSON()));
 
-    // UTILISATEUR
-    var testModelUtilisateur = new ModelUtilisateur({
-        nomComplet : "Léa Soukouti",
-        adresseMail: "lea.soukout@gmail.com",
-        motDePasse: "123456"
-    })
+// UTILISATEUR
+var testModelUtilisateur = new ModelUtilisateur({
+    nomComplet: "Léa Soukouti",
+    adresseMail: "lea.soukout@gmail.com",
+    motDePasse: "123456"
+});
 
-    testModelUtilisateur.isValid();
-    console.log("Erreurs Utilisateur: " + testModelUtilisateur.validationError);
-    testModelUtilisateur.log()
-    $(".content").html(JST['utilisateur'](testModelUtilisateur.toJSON()));
+testModelUtilisateur.isValid();
+console.log("Erreurs Utilisateur: " + testModelUtilisateur.validationError);
+testModelUtilisateur.log()
+$(".content").html(JST['utilisateur'](testModelUtilisateur.toJSON()));
+
+//CATEGORIE SPONSOR
+    var testCategorie = new ModelCategorieSponsor({
+        nom:"Papier",
+        description:"Papier"
+    });
+    testCategorie.log();
+    testCategorie.isValid();
+    console.log(testCategorie.validationError);
 });
