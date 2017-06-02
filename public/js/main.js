34d174090fd16dd49c6662df0d57ab821462f748
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 15:04:58 +0200

    deleted duplicate url

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,6 @@ $(function(){
     var testArticle = new ModelArticleDePresse({
         url: "Michelle",
         titreArticle: "Un bolomey dans l'espace",
-        url: "http://bliche.ch",
         description: "coucou",
         date: new Date("2017-03-21"),
         nomPresse: "One FM"
