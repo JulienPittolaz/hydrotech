83886027d5e8714a85f071df81068548d5eb5cdb
kelly <kelly.pala@heig-vd.ch>
2017-06-02 15:19:18 +0200

    Création de la migration Couverture

---
+++ database/migrations/2017_06_02_131413_create_actualite_edition_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActualiteEditionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actualite_edition', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('actualite_id')->unsigned();
+            $table->integer('edition_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('actualite_id')->references('id')->on('actualite')->onDelete("cascade");
+            $table->foreign('edition_id')->references('id')->on('edition')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actualite_edition');
+    }
+}
