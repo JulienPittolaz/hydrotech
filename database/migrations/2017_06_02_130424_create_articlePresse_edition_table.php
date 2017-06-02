b7ca52a833b562385daec732756d178aa20fb08d
kelly <kelly.pala@heig-vd.ch>
2017-06-02 15:08:52 +0200

    Création de la migration commentaire

---
+++ database/migrations/2017_06_02_130424_create_articlePresse_edition_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlePresseEditionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articlePresse_edition', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('articlePresse_id')->unsigned();
+            $table->integer('edition_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('articlePresse_id')->references('id')->on('articlePresse')->onDelete("cascade");
+            $table->foreign('edition_id')->references('id')->on('edition')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articlePresse_edition');
+    }
+}
