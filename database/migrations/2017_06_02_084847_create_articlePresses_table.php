4de6c22e9470cc4de64202aac26cfd04a040cedd
kelly <kelly.pala@heig-vd.ch>
2017-06-02 11:04:01 +0200

    Création des migrations users, password_resets et articlePresses

--- database/migrations/2017_06_02_084847_create_articlePresses_table.php
+++ database/migrations/2017_06_02_084847_create_articlePresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateArticlePressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articlePresses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email');
-            $table->string('password');
-            $table->string('name');
-            $table->rememberToken();
+            $table->string('url');
+            $table->string('titreArticle');
+            $table->string('description');
             $table->timestamps();
+            $table->string('nomPresse');
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articlePresses');
     }
 }
