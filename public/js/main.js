cb0f4132fdeb4dd5fb2e22d5e56f84e6ce094a7c
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 15:04:10 +0200

    Retrait des éléments tests pour la validation

--- public/js/main.js
+++ public/js/main.js
@@ -21,24 +21,13 @@ $(function(){
 
     var testArticle = new ModelArticleDePresse({
         url: "Michelle",
-        titreArticle: "Un bolomey dans l'espace"
-    });
-
-    testArticle.log();
-    console.log(testArticle.isValid());
-    testArticle.set("url",123);
-    testArticle.log();
-    console.log(testArticle.isValid());
-    testArticle.set("titreArticle","2"+123);
-    testArticle.set({
+        titreArticle: "Un bolomey dans l'espace",
         url: "http://bliche.ch",
         description: "coucou",
         date: new Date("2017-03-21"),
-        nomPresse: ""
+        nomPresse: "One FM"
     });
+
     testArticle.log();
-    console.log(testArticle.isValid());
-    console.log(testArticle.validationError);
-    console.log(testArticle.attributes.date);
 
 });
