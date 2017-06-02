a0db063a1afcb67ef179bf4bfb4f46e85c61be7e
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 16:03:11 +0200

    Création du fichier Model pour CatégorieSponsor

---
+++ public/js/Models/CategorieSponsor.js
@@ -0,0 +1,9 @@
+/**
+ * Created by timdlp on 02.06.17.
+ */
+var ModelCategorieSponsor = Hydrotech.Model.extend({
+    validate: function (attrs,options){
+        var msg = "";
+        return msg;
+    }
+});
