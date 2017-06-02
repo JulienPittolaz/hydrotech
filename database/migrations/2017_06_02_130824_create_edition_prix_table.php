6361dbc730272d7a6310e596f6bfb99ea67a8208
Julien Pittolaz <pittolazjulien@gmail.com>
2017-06-02 15:31:23 +0200

    Création de la migration Gain

---
+++ database/migrations/2017_06_02_130824_create_edition_prix_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEditionPrixTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('edition_prix', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('edition_id')->unsigned();
+            $table->integer('prix_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('prix_id')->references('id')->on('prix')->onDelete("cascade");
+            $table->foreign('edition_id')->references('id')->on('edition')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('edition_prix');
+    }
+}
