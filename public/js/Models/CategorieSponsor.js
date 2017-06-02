f7715983a38715b9757b15f720d85a97eea1a9dc
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-02 16:32:04 +0200

    Création des règles de validation pour le Model CategorieSponsor

--- public/js/Models/CategorieSponsor.js
+++ public/js/Models/CategorieSponsor.js
@@ -4,6 +4,12 @@
 var ModelCategorieSponsor = Hydrotech.Model.extend({
     validate: function (attrs,options){
         var msg = "";
+        if (_.isEmpty(attrs.nom) || !_.isString(attrs.nom)){
+            msg += "Le nom doit être du texte et ne pas être vide ! \n";
+        }
+        if (_.isEmpty(attrs.description) || !_.isString(attrs.nom)){
+            msg += "La description doit être du texte et ne pas être vide ! \n";
+        }
         return msg;
     }
 });
