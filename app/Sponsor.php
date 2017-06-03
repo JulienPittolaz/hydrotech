48e3463675aa7d8e5ec5baf3378d986c9e34c605
kelly <kelly.pala@heig-vd.ch>
2017-06-03 10:38:10 +0200

    Modification modèle Sponsor
    Ajout des fillables, du belongsToMany Categorie Sponsor et de la fonction isValid

--- app/Sponsor.php
+++ app/Sponsor.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Validator;
 
 class Sponsor extends Model
 {
-    //
+    protected $fillable = ['nom', 'urlLogo', 'urlSponsor'];
+
+
+
+    public function categorieSponsors()
+    {
+        return $this->belongsToMany('App\CategorieSponsor')->withTimestamps();
+    }
+
+    public static function isValid($data = array()){
+        return Validator::make($data, [
+            'id' => 'exists:sponsors|sometimes|required',
+            'nom'   => 'string|sometimes|required',
+            'urlLogo'    => 'url|sometimes|required',
+            'urlSponsor'    => 'url|sometimes|required',
+        ])->passes();
+    }
 }
