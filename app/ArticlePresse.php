ba17cf615d16917670e2dc362b57271be81f955c
Julien Pittolaz <pittolazjulien@gmail.com>
2017-06-03 11:07:52 +0200

    Model ArticlePresse

---
+++ app/ArticlePresse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+
+class ArticlePresse extends Model
+{
+    protected $fillable = ['url', 'titreArticle', 'description', 'dateParution', 'nomPresse'];
+
+    public function editions() {
+        return $this->belongsTo('\App\Edition');
+    }
+
+    public static function isValid($inputs) {
+        return Validator::make($inputs, [
+            'id' => 'exists:articlepresses|sometimes|required',
+            'url' => 'url|sometimes|required',
+            'titreArticle' => 'string|sometimes|required',
+            'description' => 'string|sometimes|required',
+            'dateParution' => 'date|sometimes|required',
+            'nomPresse' => 'string|sometimes|required',
+        ])->passes();
+    }
+}
