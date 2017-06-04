aa5eeae28c6a2cc16e43b517a38d137a3d55fff3
Timothée Delapierre <timothee.delapierre@gmail.com>
2017-06-04 16:45:27 +0200

    Si on vérifie qu’un objet Date est bien un objet date, il ne peut rien être d’autre. Pas besoin de faire de vérification isEmpty (d’autant que celle-ci va renvoyer true si on lui passe une date) Retournement de la condition pour les comparaisons de date, meilleure lisibilité.

--- public/js/Models/Actualite.js
+++ public/js/Models/Actualite.js
@@ -8,20 +8,11 @@ var ModelActualite = Hydrotech.Model.extend({
         if (_.isEmpty(attrs.titre)) {
             msg += 'Le titre doit être renseigné\n';
         }
-        if (_.isEmpty(attrs.datePublication)) {
-            msg += 'La date de publication doit être renseignée\n';
-        }
-        if (_.isEmpty(attrs.dateCreation)) {
-            msg += 'La date de creation doit être renseignée\n';
-        }
         if (_.isEmpty(attrs.contenu)) {
             msg += 'La section doit être renseigné\n';
         }
         if (_.isEmpty(attrs.description)) {
             msg += 'Le contenu doit être renseignée\n';
-        }
-        if (_.isEmpty(attrs.dateModification)) {
-            msg += 'La date de modification doit être renseignée\n';
         }
         if (_.isEmpty(attrs.auteur)) {
             msg += 'L\'auteur doit être renseigné\n';
@@ -58,19 +49,19 @@ var ModelActualite = Hydrotech.Model.extend({
         }
 
         // validation contraintes intégrités
-        if (attrs.publie == "true") {
+        if (attrs.publie === true) {
             attrs.datePublication <= Date.now()
         } else {
             msg += ' la date de publication doit être antérieure ou égale à la date d\'aujourdhui \n';
         }
 
         //Si une actualité est créée, alors sa date de publication est postérieure à sa date de création
-        if (!(attrs.datePublication >= attrs.dateCreation)){
+        if (attrs.datePublication <= attrs.dateCreation){
             msg += 'La date de publication doit être postérieure à la date de création \n';
         }
 
         //Si une actualité est crée, alors sa date de modification est postérieure à sa date de création
-        if (!(attrs.dateModification >= attrs.dateCreation)){
+        if (attrs.dateModification <= attrs.dateCreation){
             msg += 'La date de modification doit être postérieure à la date de création \n';
         }
 
