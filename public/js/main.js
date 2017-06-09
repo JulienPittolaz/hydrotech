3ac5bee882dc15498eeea4a7b6dd0711d423f891
Joséphine Chabin <josephine.chabin@gmail.com>
2017-06-09 09:36:03 +0200

    Blop

--- public/js/main.js
+++ public/js/main.js
@@ -823,35 +823,65 @@ var mediasTests = new ModelMedias();
 
 
 
-    var presseTests = new ModelArticlesDePresse();
-    presseTests.fetch({
-        success: function(){
-            $("#popup").html(JST['articlesPresse']({articlesPresse:presseTests.toJSON()}));
+    // var presseTests = new ModelArticlesDePresse();
+    // presseTests.fetch({
+    //     success: function(){
+    //         $("#popup").html(JST['articlesPresse']({articlesPresse:presseTests.toJSON()}));
+    //
+    //         // Call Gridder
+    //         $('.gridder').gridderExpander({
+    //             scroll: true,
+    //             scrollOffset: 30,
+    //             scrollTo: "panel", // panel or listitem
+    //             animationSpeed: 400,
+    //             animationEasing: "easeInOutExpo",
+    //             showNav: false, // Show Navigation
+    //             nextText: "", // Next button text
+    //             prevText: "", // Previous button text
+    //             closeText: "", // Close button text
+    //             onStart: function () {
+    //                 //Gridder Inititialized
+    //                 console.log('On Gridder Initialized...');
+    //             },
+    //             onContent: function () {
+    //                 //Gridder Content Loaded
+    //                 console.log('On Gridder Expand...');
+    //             },
+    //             onClosed: function () {
+    //                 //Gridder Closed
+    //                 console.log('On Gridder Closed...');
+    //             }
+    //         });
+    //     }
+    // });
 
-            // Call Gridder
-            $('.gridder').gridderExpander({
-                scroll: true,
-                scrollOffset: 30,
-                scrollTo: "panel", // panel or listitem
-                animationSpeed: 400,
-                animationEasing: "easeInOutExpo",
-                showNav: false, // Show Navigation
-                nextText: "", // Next button text
-                prevText: "", // Previous button text
-                closeText: "", // Close button text
-                onStart: function () {
-                    //Gridder Inititialized
-                    console.log('On Gridder Initialized...');
-                },
-                onContent: function () {
-                    //Gridder Content Loaded
-                    console.log('On Gridder Expand...');
-                },
-                onClosed: function () {
-                    //Gridder Closed
-                    console.log('On Gridder Closed...');
-                }
-            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var actualiteTests = new ModelActualite();
+    actualiteTests.fetch({
+        success: function(){
+            $("#popup").html(JST['actualite']({actualite:actualiteTests.toJSON()}));
         }
     });
 
