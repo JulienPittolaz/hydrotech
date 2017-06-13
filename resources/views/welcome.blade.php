a0f3c1923524dff25ea954ebb76fbfd8f2dfcfb7
LEYASKA\Léa <lea.soukouti@heig-vd.ch>
2017-06-13 09:20:16 +0200

    texte home ramnié + css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Hydrocontest - HEIG-VD</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="css/app.css" type="text/css">
@@ -48,13 +48,14 @@
     <div class="home_descConcours">
         <p> <span class="concours"> Le concours </span> <br> L’HydroContest est l’événement phare
             de la Fondation Hydros ; il est le premier
-            concours étudiant international dédié
+           <b> concours étudiant international </b> dédié
             à l’efficience énergétique nautique et
             maritime.
-            Des étudiants du monde entier doivent concevoir, fabriquer et piloter le
+            Des étudiants du monde entier doivent concevoir, <b> fabriquer et piloter le
             bateau le plus rapide et le moins gourmand
-            en matière d’énergie.
-            La Fondation fournit à chaque équipe un moteur électrique et une batterie identiques. Les navires concourent dans 2 catégories distinctes: celle du Transport de Masse, qui simule le déplacement d'un cargo avec une charge de 200kg et celle des Transports Légers, qui représente le transport de personnes sur un bateau de plaisance (embarcation personnelle) avec seulement 20kg à bord. Une 3ème course, la« Long Distance Race», confronte tous les bateaux de la catégorie Transport Léger et attribuera la victoire au navire ayant parcouru la plus longue distance pendant deux heures de navigation.
+            en matière d’énergie </b>.
+            La Fondation fournit à chaque équipe un moteur électrique et une batterie identiques. Les navires concourent dans 2 catégories distinctes: celle du <b>Transport de Masse</b>, qui simule le déplacement d'un cargo avec une charge de 200kg et celle des <b>Transports Légers</b>,
+            qui représente le transport de personnes sur un bateau de plaisance (embarcation personnelle) avec seulement 20kg à bord. Une 3ème course, la <b> « Long Distance Race » </b>, attribuera la victoire au navire (léger) ayant parcouru la plus longue distance pendant deux heures de navigation.
         </p>
         <p class="home_more"><a target="_blank" href="http://www.hydrocontest.org/fr/">+ En savoir plus</a></p>
     </div>
