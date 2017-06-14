fda8aafb293a96ba22a5bf4b6bca2337dc2c53e0
kelly <kelly.pala@heig-vd.ch>
2017-06-14 15:39:37 +0200

    Modification back office
    Modification back office, les tables ne sont affichées que si elles contiennent des données

--- resources/views/editionAssociation/index.blade.php
+++ resources/views/editionAssociation/index.blade.php
@@ -19,35 +19,37 @@
                             {{ Session::get('error') }}
                         </div>
                     @endif
-                    <ul>
-                    @foreach($editions as $edition)
-                            <li>{{$edition->annee}}</li>
-                            <ul>
+                    @if($editions != null)
 
-                                @foreach($edition->objetsDeLedition as $objet)
-                                    @if($objet->pivot->actif == true)
-                                        <li>{{$objet->titre}}{{$objet->nom}}{{$objet->titreArticle}}
-                                            <form class="bouton_delete" method="post"
-                                                  action="{{action('Back_office\EditionAssociationCtrl@destroy', ['edition_id' => $edition->id, 'type_ressource' => $typeRessource, 'resource_id' => $objet->id])}}"
-                                                  accept-charset="UTF-8">
-                                                <input type="hidden" name="_method" value="DELETE">
-                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                                <button type="submit" class="btn bg-red">X
-                                                </button>
-                                            </form>
-                                        </li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        <a href="{{action('Back_office\EditionAssociationCtrl@create', [$edition->annee, $typeRessource])}}"
-                           target="_parent">
-                            <button type="button" class="m-b-20 btn bg-green waves-effect">NOUVEAU</button>
-                        </a>
+                        <ul>
+                            @foreach($editions as $edition)
+                                <li>{{$edition->annee}}</li>
+                                <ul>
 
-                        @endforeach
-                    </ul>
+                                    @foreach($edition->objetsDeLedition as $objet)
+                                        @if($objet->pivot->actif == true)
+                                            <li>{{$objet->titre}}{{$objet->nom}}{{$objet->titreArticle}}
+                                                <form class="bouton_delete" method="post"
+                                                      action="{{action('Back_office\EditionAssociationCtrl@destroy', ['edition_id' => $edition->id, 'type_ressource' => $typeRessource, 'resource_id' => $objet->id])}}"
+                                                      accept-charset="UTF-8">
+                                                    <input type="hidden" name="_method" value="DELETE">
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                    <button type="submit" class="btn bg-red">X
+                                                    </button>
+                                                </form>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                                <a href="{{action('Back_office\EditionAssociationCtrl@create', [$edition->annee, $typeRessource])}}"
+                                   target="_parent">
+                                    <button type="button" class="m-b-20 btn bg-green waves-effect">NOUVEAU</button>
+                                </a>
 
+                            @endforeach
+                        </ul>
 
+                    @endif
                 </div>
             </div>
         </div>
