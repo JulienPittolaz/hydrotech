32632eae475909ff31ce4b93a776307fbce885f8
Julien Pittolaz <pittolazjulien@gmail.com>
2017-06-15 15:08:22 +0200

    mail

--- public/js/Controllers/Contact.js
+++ public/js/Controllers/Contact.js
@@ -77,7 +77,7 @@ var CtrlContact = {
     },
     send: function(data){
         console.log(data);
-        $.post( "send/email/", data , function() {
+        $.post( "./send/email/", data , function() {
             $('.message_success').show();
             }
         );
